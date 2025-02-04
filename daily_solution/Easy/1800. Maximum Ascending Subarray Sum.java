36098a601c7d73370bf59510fbffe7242388bd45
Balamurugan V <balamurugankumar880@gmail.com>
2025-02-04 20:55:00 +0530

    Array

--- daily_solution/Easy/1800. Maximum Ascending Subarray Sum.java
+++ daily_solution/Easy/1800. Maximum Ascending Subarray Sum.java
@@ -3,15 +3,19 @@ class Solution {
         //length of nums array are stored in length
         int length=nums.length;
         int max_subarray_sum=0;
-        //start loop
+        //start loop for creating subarray
         for(int start=0;start<length;start++){
             int subarray_sum=nums[start];
-            int end;
-            for(end=start+1;end<length && nums[end]>nums[end-1];end++){
+            int end=start+1;
+            //start another while loop for sum of subarray 
+            while(end<length && nums[end]>nums[end-1]){
                 subarray_sum+=nums[end];
+                end+=1;
             }
+            //use Math.max buit-in function for find maximum subaray sum
             max_subarray_sum=Math.max(max_subarray_sum,subarray_sum);
         }
+        //return the maximum subarray sum 
         return max_subarray_sum;
         
     }
