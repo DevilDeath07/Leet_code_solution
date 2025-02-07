aeb2464305cc351cab55c5a1ba104cc983af4b81
Balamurugan V <balamurugankumar880@gmail.com>
2025-02-07 09:35:33 +0530

    Array, Hash Table

--- daily_solution/Easy/1. Two Sum.py
+++ daily_solution/Easy/1. Two Sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #length of the given array is stored in n
+        #length of the given array is stored in n 
         n=len(nums)
         result=[]
         for i in range(0,n,1):
