3fda62c4afac8b0d18327764759727ad05c1992e
Balamurugan V <balamurugankumar880@gmail.com>
2025-05-05 21:33:08 +0530

    math, binary search
    This solution beats 100% accepted in that platform...

---
+++ daily_solution/Easy/69. Sqrt(x).py
@@ -0,0 +1,8 @@
+#import math module
+import math
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # take squlare root of the given number using sqrt() function and convert into integer 
+        return int(sqrt(x))
+
+        
