d3057679d6420a0030c086866a85b035ed1dd192
Balamurugan V <balamurugankumar880@gmail.com>
2025-09-01 20:02:22 +0530

    Array...

---
+++ daily_solution/Easy/1470. Shuffle the Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        arr1 = nums[:n]
+        arr2 = nums[n:]
+        result = []
+        for i in range(n):
+            result.append(arr1[i])
+            result.append(arr2[i])
+        return result
