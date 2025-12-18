4ef1081eb0f019f512c61634dd35493ec77cb8b9
Balamurugan V <balamurugankumar880@gmail.com>
2025-12-18 13:24:41 +0530

    Add solution for counting bits in integers
    Topic:
    
    Dynamic programming and Bit Manipulation...

---
+++ daily_solution/Easy/338. Counting Bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        result = []
+        for i in range(n+1):
+            binary = f"{i:b}"
+            result.append(binary.count("1"))
+        return result
