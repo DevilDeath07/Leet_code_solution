c012161baab6513f6723c06ca65a7d13f488d758
Balamurugan V <balamurugankumar880@gmail.com>
2025-12-20 19:48:22 +0530

    Refactor minDeletionSize to check sorted columns
    Refactor column checking logic and build column strings for sorting validation.

--- daily_solution/Easy/944. Delete Columns to Make Sorted.py
+++ daily_solution/Easy/944. Delete Columns to Make Sorted.py
@@ -1,13 +1,21 @@
+from typing import List
+
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
         r = len(strs)
         c = len(strs[0])
         count = 0
+        grid = []
 
-        # Check each column
-        for col in range(c):
-            for row in range(1, r):
-                if strs[row][col] < strs[row-1][col]:
-                    count += 1
-                    break   # No need to check further in this column
+        # Build each column string
+        for j in range(c):          # loop over columns
+            temp = ""
+            for i in range(r):      # loop over rows
+                temp += strs[i][j]  
+            grid.append(temp)
+
+        # Check if each column string is sorted
+        for word in grid:
+            if word != ''.join(sorted(word)):
+                count += 1
         return count
