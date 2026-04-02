d870d067a166f05abe83c62fc6ef970dac89fdb0
Balamurugan V <balamurugankumar880@gmail.com>
2026-04-02 19:45:53 +0530

    Add solution for finding the duplicate number
    Topic:
    
    Array, Two pointer, bit manipulation and binary search...

---
+++ daily_solution/Medium/287. Find the Duplicate Number.py
@@ -0,0 +1,7 @@
+lass Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
